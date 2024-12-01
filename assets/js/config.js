a16ce37362e8ffc4a1b4df61b182f083792d414b
OasisTwink <binarybritish09@gmail.com>
2024-12-01 21:17:39 +0000

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,20 +4,20 @@ const BADGES = [
     image: "assets/badges/hypesquad_bravery.svg",
   },
   {
-    name: "Subscriber since Jun 29, 2022",
+    name: "Subscriber since Apr 1, 2001",
     image: "assets/badges/nitro.svg",
   },
   {
-    name: "Server boosting Jun 29, 2022",
+    name: "Server boosting Jun 9, 1939",
     image: "assets/badges/boost.svg",
   },
   {
-    name: "That's an example! Change in config.js", // Message that appears when you hover over it
+    name: "Haii :3", // Message that appears when you hover over it
     image: "assets/badges/bug_hunter_level2.svg", // Icon (in .svg) of the badge that is in the "assets/badges" you choose
   },
 ];
 
-const USER_ID = "277375966341496832"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
+const USER_ID = "1245873601987481683"; // Your Discord Id Here | You need to join Lanyard Discord for it to work! [https://discord.gg/z2xW3zxYdt]
 const BACKGROUND_IMAGE = "assets/images/star.gif"; // Here is the background of the site, behind your profile
 const STATUS = {
   dnd: {
